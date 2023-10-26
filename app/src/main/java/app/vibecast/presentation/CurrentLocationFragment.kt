205d053707551b79832a0e2c309592bffb6b820e
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-10-25 21:38:53 -0700

    Dvivided homescreen into subcomponents and refined over all layout

--- app/src/main/java/app/vibecast/presentation/CurrentLocationFragment.kt
+++ app/src/main/java/app/vibecast/presentation/CurrentLocationFragment.kt
@@ -1,11 +1,11 @@
 package app.vibecast.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import app.vibecast.R
+import androidx.fragment.app.Fragment
+import app.vibecast.databinding.FragmentCurrentLocationBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +18,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CurrentLocationFragment : Fragment() {
+
+    private lateinit var binding : FragmentCurrentLocationBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,9 +36,13 @@ class CurrentLocationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_location, container, false)
+        binding = FragmentCurrentLocationBinding.inflate(inflater,container,false)
+
+        return binding.root
     }
+
+
+
 
     companion object {
         /**
