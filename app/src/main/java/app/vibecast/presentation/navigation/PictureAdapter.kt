bb434e2f08dd8694c23406b2f149da27fab913e8
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-10-28 16:58:22 -0700

    Implemented picture gallery screen and refined home screen

---
+++ app/src/main/java/app/vibecast/presentation/navigation/PictureAdapter.kt
@@ -0,0 +1,53 @@
+package app.vibecast.presentation.navigation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import app.vibecast.databinding.ItemCardviewBinding
+import app.vibecast.domain.entity.Picture
+
+// Replace with the correct package name
+
+class PictureAdapter(private val items: List<Picture>) : RecyclerView.Adapter<PictureAdapter.PictureViewHolder>() {
+
+    class PictureViewHolder(private val binding: ItemCardviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        // Use binding to access views in the layout
+        val savedImage = binding.savedImage
+        val header = binding.header
+        val title = binding.title
+        val description = binding.description
+        val removeButton = binding.removeBtn
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemCardviewBinding.inflate(inflater, parent, false)
+        return PictureViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+        val item = items[position]
+
+        // Bind data to views using ViewBinding
+        holder.savedImage.setImageResource(item.imageResId)
+        holder.header.text = item.headerText
+        holder.title.text = item.titleText
+        holder.description.text = item.descriptionText
+
+
+//        holder.removeButton.setOnClickListener {
+//            //TODO
+//        }
+
+        // Set click listeners or other actions as needed
+
+
+    }
+}
+
