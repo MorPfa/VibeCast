eb7f5e50958f9ee91999432c3220783fd304c3c8
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-10-30 15:12:10 -0700

    Added unit tests for weather repository

--- app/src/test/java/app/vibecast/domain/GetWeatherUseCaseTest.kt
+++ app/src/test/java/app/vibecast/domain/GetWeatherUseCaseTest.kt
@@ -1,13 +1,11 @@
 package app.vibecast.domain
 
-import app.vibecast.domain.entity.Weather
 import app.vibecast.domain.repository.WeatherRepository
 import app.vibecast.domain.usecase.GetWeatherUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.kotlin.mock
@@ -24,7 +22,6 @@ class GetWeatherUseCaseTest {
     fun testProcess()= runTest {
         val request = GetWeatherUseCase.Request(0)
         val fakeWeather = CreateFakeWeather().createFakeWeather()
-        val weather = fakeWeather
         whenever(weatherRepository.getWeather(request.weatherDataId)).thenReturn(flowOf(fakeWeather))
         val response = useCase.process(request).first()
         assertEquals(GetWeatherUseCase.Response(fakeWeather),response)
