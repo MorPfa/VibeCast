b4df08ed200162c4f22bf40d24ee6098ed7ce7f9
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-10-30 15:12:10 -0700

    Added unit tests for weather repository

--- app/src/main/java/app/vibecast/data/data_repository/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/app/vibecast/data/data_repository/repository/WeatherRepositoryImpl.kt
@@ -11,12 +11,11 @@ import javax.inject.Inject
 class WeatherRepositoryImpl @Inject constructor(
     private val remoteWeatherDataSource: RemoteWeatherDataSource,
     private val localWeatherDataSource: LocalWeatherDataSource
-    //TODO add localWeatherDataSource to get cached weather data
 ) : WeatherRepository{
     override fun getWeather(id : Int ): Flow<Weather> = remoteWeatherDataSource.getWeather()
         .onEach {
             localWeatherDataSource.addWeather(id)
         }
-
+//TODO Once workmanager is implemented add refresh weather function to do what getWeather does right now and change getweather to only use the locally persisted data
 
     }
---
+++ app/src/test/java/app/vibecast/data/WeatherRepositoryImplTest.kt
@@ -0,0 +1,33 @@
+package app.vibecast.data
+
+import app.vibecast.data.data_repository.data_source.local.LocalWeatherDataSource
+import app.vibecast.data.data_repository.data_source.remote.RemoteWeatherDataSource
+import app.vibecast.data.data_repository.repository.WeatherRepositoryImpl
+import app.vibecast.domain.CreateFakeWeather
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+class WeatherRepositoryImplTest {
+
+    private val remoteWeatherDataSource = mock<RemoteWeatherDataSource>()
+    private val localWeatherDataSource = mock<LocalWeatherDataSource>()
+    private val repositoryImpl = WeatherRepositoryImpl(remoteWeatherDataSource,localWeatherDataSource)
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun testGetWeather() = runTest {
+        val weather = CreateFakeWeather().createFakeWeather()
+        whenever(remoteWeatherDataSource.getWeather()).thenReturn(flowOf(weather))
+        val result = repositoryImpl.getWeather(0).first()
+        assertEquals(weather, result)
+        verify(localWeatherDataSource).addWeather(0)
+    }
+}
