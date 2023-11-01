5079a90736eba02ecf0e9eda9c8097a512972dbc
MorPfa <moritzpfatteicher@gmail.com>
2023-10-31 21:59:27 -0700

    fixed string resources to correctly hold values from use cases, continued implementing converter and viewmodel

--- app/src/main/java/app/vibecast/presentation/weather/WeatherConverter.kt
+++ app/src/main/java/app/vibecast/presentation/weather/WeatherConverter.kt
@@ -1,14 +1,24 @@
 package app.vibecast.presentation.weather
 
 import android.content.Context
+import app.vibecast.R
 import app.vibecast.domain.usecase.GetWeatherUseCase
 import app.vibecast.presentation.state.CommonResultConverter
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
+
 class WeatherConverter @Inject constructor(@ApplicationContext private val context: Context) :
     CommonResultConverter<GetWeatherUseCase.Response, WeatherModel>() {
     override fun convertSuccess(data: GetWeatherUseCase.Response): WeatherModel {
-        return WeatherModel()
+        return WeatherModel(
+            context.getString(R.string.center_location_text, data.weather.cityName),
+            data.weather.latitude,
+            data.weather.longitude,
+            data.weather.currentWeather,
+            data.weather.hourlyWeather
+
+//TODO figure out how to change data arrangement to make it work like in 8.02
+        )
     }
 }
--- app/src/main/java/app/vibecast/presentation/weather/WeatherViewModel.kt
+++ app/src/main/java/app/vibecast/presentation/weather/WeatherViewModel.kt
@@ -1,9 +1,15 @@
 package app.vibecast.presentation.weather
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import app.vibecast.domain.usecase.GetWeatherUseCase
+import app.vibecast.presentation.state.UiState
 import app.vibecast.presentation.weather.WeatherConverter
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -14,6 +20,22 @@ class WeatherViewModel @Inject constructor(
 ) : ViewModel() {
 
 
+    //TODO fix this
 
+    private val _weatherFlow =
+        MutableStateFlow<UiState<WeatherModel>>(UiState.Loading)
+    val weatherFlow: StateFlow<UiState<WeatherModel>> = _weatherFlow
+
+    fun loadUser(userId: Long) {
+        viewModelScope.launch {
+            weatherUseCase.execute(GetWeatherUseCase.Request())
+                .map {
+                    weatherConverter.convert(it)
+                }
+                .collect {
+                    _weatherFlow.value = it
+                }
+        }
+    }
 
 }
