4f5f49aa635d21f0ef88a864e3b66eb7e52fdb56
MorPfa <moritzjohannes.pfa1@seattlecolleges.edu>
2023-10-31 17:17:00 -0700

    Created Unit tests for data persistence package and fixed failing test for api connection by changin out call to dummy data creation class method for unique dummy data entity

--- app/src/test/java/app/vibecast/data/RemoteWeatherDataSourceImplTest.kt
+++ app/src/test/java/app/vibecast/data/RemoteWeatherDataSourceImplTest.kt
@@ -1,27 +1,133 @@
 package app.vibecast.data
 
 import app.vibecast.BuildConfig
+import app.vibecast.data.local.db.weather.WeatherEntity
 import app.vibecast.data.remote.network.weather.CoordinateApiModel
+import app.vibecast.data.remote.network.weather.CurrentWeatherRemote
+import app.vibecast.data.remote.network.weather.HourlyWeatherRemote
+import app.vibecast.data.remote.network.weather.WeatherApiModel
+import app.vibecast.data.remote.network.weather.WeatherConditionRemote
 import app.vibecast.data.remote.network.weather.WeatherService
 import app.vibecast.data.remote.source.RemoteWeatherDataSourceImpl
-import app.vibecast.domain.CreateFakeWeather
+import app.vibecast.domain.entity.CurrentWeather
+import app.vibecast.domain.entity.HourlyWeather
 import app.vibecast.domain.entity.UseCaseException
+import app.vibecast.domain.entity.Weather
+import app.vibecast.domain.entity.WeatherCondition
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
-import kotlinx.coroutines.flow.collect
 
 
 class RemoteWeatherDataSourceImplTest {
 
     private val weatherService = mock<WeatherService>()
     private val weatherDataSource = RemoteWeatherDataSourceImpl(weatherService)
+    private lateinit var remoteWeather: WeatherApiModel
+    private lateinit var  expectedWeather : Weather
+    private val cityName = "London"
+
+
+    @Before
+    fun setUp() {
+         remoteWeather = WeatherApiModel(
+            cityName = "London",
+            latitude = 51.5074,
+            longitude = -0.1278,
+            currentWeatherRemote = CurrentWeatherRemote(
+                timestamp = 1637094000,
+                temperature = 15.0,
+                feelsLike = 14.0,
+                humidity = 70,
+                uvi = 5.2,
+                cloudCover = 40,
+                visibility = 10,
+                windSpeed = 12.0,
+                weatherConditionRemotes = listOf(
+                    WeatherConditionRemote(
+                        conditionId = 800,
+                        mainDescription = "Clear",
+                        detailedDescription = "Clear sky",
+                        icon = "01d"
+                    )
+                )
+            ),
+            hourlyWeather = List(24) {
+                HourlyWeatherRemote(
+                    timestamp = (1637094000 + it * 3600).toLong(),
+                    temperature = 14.0,
+                    feelsLike = 13.0,
+                    humidity = 65,
+                    uvi = 5.5,
+                    cloudCover = 45,
+                    windSpeed = 11.0,
+                    weatherConditionRemotes = listOf(
+                        WeatherConditionRemote(
+                            conditionId = 800,
+                            mainDescription = "Clear",
+                            detailedDescription = "Clear sky",
+                            icon = "01d"
+                        )
+                    ),
+                    chanceOfRain = 10.0
+                )
+            }
+        )
+
+        expectedWeather =  Weather(
+            cityName = cityName,
+            latitude = 51.5074,
+            longitude = -0.1278,
+            currentWeather = CurrentWeather(
+                timestamp = 1637094000,
+                temperature = 15.0,
+                feelsLike = 14.0,
+                humidity = 70,
+                uvi = 5.2,
+                cloudCover = 40,
+                visibility = 10,
+                windSpeed = 12.0,
+                weatherConditions = listOf(
+                    WeatherCondition(
+                        conditionId = 800,
+                        mainDescription = "Clear",
+                        detailedDescription = "Clear sky",
+                        icon = "01d"
+                    )
+                )
+            ),
+            hourlyWeather = List(24) {
+                HourlyWeather(
+                    timestamp = 1637094000,
+                    temperature = 14.0,
+                    feelsLike = 13.0,
+                    humidity = 65,
+                    uvi = 5.5,
+                    cloudCover = 45,
+                    windSpeed = 11.0,
+                    weatherConditions = listOf(
+                        WeatherCondition(
+                            conditionId = 800,
+                            mainDescription = "Clear",
+                            detailedDescription = "Clear sky",
+                            icon = "01d"
+                        )
+                    ),
+                    chanceOfRain = 10.0
+                )
+            }
+        )
+
+    }
+
 
 
     @ExperimentalCoroutinesApi
@@ -42,8 +148,6 @@ class RemoteWeatherDataSourceImplTest {
 
         whenever(weatherService.getCiyCoordinates(cityName, 1, BuildConfig.OWM_KEY)).thenReturn(remoteCoordinates)
 
-        val remoteWeather = CreateFakeWeatherResponse().createFakeWeatherApiModel()
-        val expectedWeather = CreateFakeWeather().createFakeWeather()
         whenever(weatherService.getWeather(remoteCoordinates.latitude, remoteCoordinates.longitude, BuildConfig.OWM_KEY)).thenReturn(remoteWeather)
 
         val result = weatherDataSource.getWeather(cityName).first()
